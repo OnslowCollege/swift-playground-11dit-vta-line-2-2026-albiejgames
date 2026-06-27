0e5229c8ac70e5c68bf29f9e605d5631df87c843
albiejgames <albert.oreilly@student.onslow.school.nz>
2026-06-27 06:14:19 +0000

    Jun 27, 2026, 6:14 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -19,7 +19,7 @@ struct SwiftPlayground {
         weekDays.forEach { weekDay in
             print("On",weekDay,",how many hours did you spend on:")
 
-            let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord","Other"]
+            //let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord","Other"]
 
             print("Facebook:")
             let facebook = readLine()!
