610db318fc4adb904e9a35de586baaa09c790344
albiejgames <albert.oreilly@student.onslow.school.nz>
2026-07-01 09:08:56 +0000

    Added foreach loop back in

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -16,21 +16,23 @@ struct SwiftPlayground {
         //set up weekday array
         let weekDays: [String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
-        print("Facebook:")
-        //get user input for facebook
-        var userInput = readLine()! 
-        //convert to an integer
-        var facebook = Int(userInput)! 
-        //print(facebook)
-        print("Snapchat:")
-        //get user input for snapchat
-        var snapchatInput = readLine()!
-        //convert to integer
-        var snapchat = Int(snapchatInput)!
-        //print(snapchat)
-        //add facebook and snapchat together
-        var timeSpent = (facebook + snapchat) 
-        print("Timespent is \(timeSpent)")
+        weekDays.forEach { weekDay in
+            print("On",weekDay,",how many hours did you spend on:")
+            print("Facebook:")
+            //get user input for facebook
+            var userInput = readLine()! 
+            //convert to an integer
+            var facebook = Int(userInput)! 
+            //print(facebook)
+            print("Snapchat:")
+            //get user input for snapchat
+            var snapchatInput = readLine()!
+            //convert to integer
+            var snapchat = Int(snapchatInput)!
+            //print(snapchat)
+            //add facebook and snapchat together
+            var timeSpent = (facebook + snapchat) 
+            print("Timespent is \(timeSpent)")
+        }
     }
-
-    }
+}
