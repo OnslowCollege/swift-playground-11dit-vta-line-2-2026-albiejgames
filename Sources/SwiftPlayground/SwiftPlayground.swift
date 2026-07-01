9ba0879ca95f0ccefbe12093a3a276e67985971b
albiejgames <albert.oreilly@student.onslow.school.nz>
2026-07-01 09:01:37 +0000

    Converted user input to integers

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -17,12 +17,20 @@ struct SwiftPlayground {
         let weekDays: [String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
         print("Facebook:")
-        //need to get these as integers
-        let facebook = readLine()!
+        //get user input for facebook
+        var userInput = readLine()! 
+        //convert to an integer
+        var facebook = Int(userInput)! 
+        //print(facebook)
         print("Snapchat:")
-        let snapchat = readLine()!
-        let timeSpent = (facebook + snapchat) 
-        print(timeSpent)
+        //get user input for snapchat
+        var snapchatInput = readLine()!
+        //convert to integer
+        var snapchat = Int(snapchatInput)!
+        //print(snapchat)
+        //add facebook and snapchat together
+        var timeSpent = (facebook + snapchat) 
+        print("Timespent is \(timeSpent)")
     }
 
     }
