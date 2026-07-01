1c809181c6f9c85ad839f98a4873d10bd3768b66
albiejgames <albert.oreilly@student.onslow.school.nz>
2026-07-01 08:42:02 +0000

    Tried to do maths on time spent

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -14,20 +14,15 @@ struct SwiftPlayground {
         print ("")
         
         //set up weekday array
-        let weekDays: [String] = ["Monday","Tuesday","Wednesday"]
-
-        weekDays.forEach { weekDay in
-            print("On",weekDay,",how many hours did you spend on:")
-
-            //let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord","Other"]
-
-            print("Facebook:")
-            //need to get these as integers
-            let facebook = readLine()!
-            print("Snapchat:")
-            let snapchat = readLine()!
-            let timeSpent = (facebook + snapchat)
-            print("Overall you spent:", timeSpent)
-        }
+        let weekDays: [String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
+        print("Facebook:")
+        //need to get these as integers
+        let facebook = readLine()!
+        print("Snapchat:")
+        let snapchat = readLine()!
+        let timeSpent = (facebook + snapchat) 
+        print(timeSpent)
     }
-}
+
+    }
