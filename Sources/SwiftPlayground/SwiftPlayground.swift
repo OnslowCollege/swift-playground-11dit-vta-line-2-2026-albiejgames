51b4efabb4abfec56f020841224aa1f703936630
albiejgames <albert.oreilly@student.onslow.school.nz>
2026-07-05 08:09:56 +0000

    Added new foreach loops and summary

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,32 +7,44 @@ struct SwiftPlayground {
         print("Welcome to the screen time tracker!")
         print("")
         print ("What is the time limit set for each day?")
-
-        let timeLimit = readLine()!
-        // todo: need to make sure this is a number
-        //print(timeLimit)
+        var timeLimit = Double(readLine()!)!
+        //check that time is less than 24 hours
+        while timeLimit > 24 {
+            print ("Time must be less than 24 hours")
+            timeLimit = Double(readLine()!)!
+        }
         print ("")
         
         //set up weekday array
-        let weekDays: [String] = ["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        let weekDays: [String] = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
         let socialMedia: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
+        //capture hours spent per day
+        var hoursSpent : [Int] = []
         weekDays.forEach { weekDay in
+            var timeSpent: Int = 0
             print("On",weekDay,",how many hours did you spend on:")
-            print("Facebook:")
-            //get user input for facebook
-            var userInput = readLine()! 
-            //convert to an integer
-            var facebook = Int(userInput)! 
-            //print(facebook)
-            print("Snapchat:")
-            //get user input for snapchat
-            var snapchatInput = readLine()!
-            //convert to integer
-            var snapchat = Int(snapchatInput)!
-            //print(snapchat)
-            //add facebook and snapchat together
-            var timeSpent = (facebook + snapchat) 
-            print("Timespent is \(timeSpent)")
+            socialMedia.forEach { social in
+                print(social)
+                let social: Int = Int(readLine()!)!
+                timeSpent += social
+                //todo: check not over 24 hours 
+            }
+            print("You spent \(timeSpent) hours total on Social Media today!")
+            //add time to array so we can see the hours per week
+            hoursSpent.append(timeSpent)
+            //print(hoursSpent)
         }
+        let totalHours = Double(hoursSpent.reduce(0, +))
+        let average = Double(totalHours/7)
+        var limit = "OVER"
+        if average < timeLimit {
+            limit = "UNDER"
+        }
+        print("")
+        print("Summary:")
+        print (" ")
+        print("- This week you spent \(totalHours) hours doom scrolling")
+        print("The average time was \(average) hours")
+        print("Overall your time was \(limit) the limit")
     }
 }
